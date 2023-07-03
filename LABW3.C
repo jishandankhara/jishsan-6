46333ebeb200b465ba8c01a2898c62342df453b3
jishandankhara <136707738+jishandankhara@users.noreply.github.com>
2023-07-03 14:35:07 +0530

    Add files via upload

---
+++ LABW3.C
@@ -0,0 +1,12 @@
+//write a program to print the wultiplication tabel of the number N using any of lopp.
+#include<stdio.h>
+#include<conio.h>
+void main(){
+int a,b;
+clrscr();
+printf("enter a number:");
+scanf("%d",&a);
+for(b=1;b<=10;b++){
+   printf(" %d X %d = %d\n",a,b,a*b);
+   }
+   getch();}
